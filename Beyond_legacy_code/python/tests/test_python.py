f4e45879552ad9b2a1b8ea0d78ad5ba7d2a5de69
kikugawa-shoma <kikugawa.shukatsu@gmail.com>
2022-07-11 21:59:26 +0900

    add first test

---
+++ Beyond_legacy_code/python/tests/test_python.py
@@ -0,0 +1,14 @@
+from app.person import Person
+
+import unittest
+
+
+class TestPerson(unittest.TestCase):
+    def setUp(self):
+        self.personName = "Bob"
+        self.personAge = 21
+
+    def testCreatePersonWithNameAndAge(self):
+        person = Person(self.personName, self.personAge)
+        self.assertEqual(self.presonName, person.getName())
+        self.assertEqual(self.personAge, person.getAge())
